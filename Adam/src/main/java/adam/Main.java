4abc8f2e8822f2cf0d8c14d41acc307dfbeb3fdb
Razvan-Gabriel Geangu <k1502791@kcl.ac.uk>
2016-12-02 14:36:19 +0000

    Add main for view

---
+++ Adam/src/main/java/adam/Main.java
@@ -0,0 +1,28 @@
+package adam;
+	
+import adam.view.MainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			MainView root = new MainView();
+			Scene scene = new Scene(root,800,560);
+			scene.getStylesheets().add(getClass().getResource("view/application.css").toExternalForm());
+			
+			primaryStage.setTitle("Adam - Your Macro-Economics Friend");
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- Adam/src/main/java/adam/MainApp.java
+++
@@ -1,9 +0,0 @@
-package adam;
-
-public class MainApp
-{
-	public static void main(String[] args)
-	{
-		
-	}
-}
