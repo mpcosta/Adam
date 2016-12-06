ca77c82f6efc9bf9a431beab22c07ad07b9d7694
Michael <michael.peecock@kcl.ac.uk>
2016-12-06 13:31:26 +0000

    Rename Main to MainApp

--- Adam/src/main/java/adam/MainApp.java
+++ Adam/src/main/java/adam/MainApp.java
@@ -6,7 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
