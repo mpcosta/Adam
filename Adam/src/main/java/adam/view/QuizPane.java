ad18358fc85fb39e6f3e190686cd9c1b7f05a705
Abdo, Hager (k1504324) <hager.abdo@kcl.ac.uk>
2016-12-14 13:33:55 +0400

    Update QuizPane.java

--- Adam/src/main/java/adam/view/QuizPane.java
+++ Adam/src/main/java/adam/view/QuizPane.java
@@ -16,7 +16,9 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class QuizPane extends StackPane {
-
+/**
+	 * Variables for the labels and boxes related to the multiple choice questions 
+	 */
 	private Label questionLabel;
 	private Label resLabel;
 	private Button submitButton;
@@ -32,7 +34,12 @@ public class QuizPane extends StackPane {
 	public HashMap<String, ArrayList<Integer>> getCorrectAnswers() {
 		return questionsRes.getCorrectAnswers();
 	}
-	
+	/**
+	 * A constructor to set the question label on the main view and pad it to the center 
+	 * Sets the submit button to be located below the question and disables it 
+	 * Calls a list of radio buttons and saves it as answers list 
+	 * Sets the question box as a vertical box and aligns it to the center 
+	 */
 	public QuizPane() {
 		currentQuestion = 0;
 		questionsRes = new Questions();
@@ -55,13 +62,21 @@ public class QuizPane extends StackPane {
 		
 		getChildren().add(questionBox);
 	}
-	
+	/**
+	 * A method to change the question by clearing the current question and reseting the submit button to be disabled 
+	 * Sets the resLabel to Feedback 
+	 * @param question
+	 * @param answers
+	 * @param correctAnswerIndices
+	 */
 	public void changeQuestion(String question, ArrayList<String> answers, ArrayList<Integer> correctAnswerIndices) {
 		questionLabel.setText(question);
 		resLabel.setText("Feedback");
 		
 		answersList.clear();
-		
+		/**
+		 * To space the answers and retrieve the specific number of answers for each question from the Questions class 
+		 */
 		for (int i = 0; i < answers.size(); ++i){
 			CheckBox one = new CheckBox(answers.get(i));
 			
@@ -74,32 +89,49 @@ public class QuizPane extends StackPane {
 		
 		submitButton.setDisable(true);
 	}
-	
+	/**
+	 * A getter for the submit button for the question 
+	 * @return the submit button 
+	 */
 	public Button getButton () {
 		return submitButton;
 	}
-	
+	/**
+	 * A getter for the resLabel 
+	 * @return the resLabel 
+	 */
 	public Label getResLabel() {
 		return resLabel;
 	}
-	
+	/**
+	 * A getter for the current question being displayed to the user 
+	 * @return the current question 
+	 */
 	public int getCurrentQuestionIndex() {
 		return currentQuestion;
 	}
-
+/**
+	 * A method to clear the question and all that apply to it 
+	 */
 	private void clearQuestionBox() {
 		questionBox.getChildren().clear();
 		questionBox.getChildren().add(questionLabel);
 		questionBox.getChildren().addAll(answersList);
 		questionBox.getChildren().addAll(submitButton, resLabel);
 	}
-	
+	/**
+	 * An action handler for the collection of radio buttons in the answer list for each question 
+	 * @param handler
+	 */
 	public void setHandlersForAnswers(EventHandler<ActionEvent> handler) {
 		for (CheckBox checkBox : answersList) {
 			checkBox.setOnAction(handler);
 		}
 	}
-	
+	/**
+	 * A getter method for the answers list as an array list of check boxes 
+	 * @return the answers list 
+	 */
 	public ArrayList<CheckBox> getAnswersList() {
 		return answersList;
 	}
