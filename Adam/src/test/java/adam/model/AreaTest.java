efc489cda3192bd88fb381bc164453e419e24c2d
Michael <michael.peecock@kcl.ac.uk>
2016-12-14 15:35:07 +0000

    Add more tests.

--- Adam/src/test/java/adam/model/AreaTest.java
+++ Adam/src/test/java/adam/model/AreaTest.java
@@ -4,13 +4,19 @@ import static adam.model.Area.cacheAllNames;
 import static adam.model.Area.codeIsValid;
 import static adam.model.Area.estimateNamesFromFragment;
 import static adam.model.Area.getAllNames;
+import static adam.model.Area.getAreaFromCode;
+import static adam.model.Area.getAreaCodeFromName;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 import adam.model.Area.Code;
+import adam.model.Country;
+import adam.model.Region;
 
 public class AreaTest {
 	
@@ -33,27 +39,6 @@ public class AreaTest {
 		assertThat(getAllNames().isEmpty(), is(false));
 	}
 	
-	/*
-	@Test
-	public void testCacheAllNames() {
-		final long duration = getDurationOfGetAllNames();
-		
-		cacheAllNames();
-		
-		assertThat(getDurationOfGetAllNames() < duration, is(true));
-	}
-	*/
-	
-	private long getDurationOfGetAllNames() {
-		final long startTime = System.nanoTime();
-		
-		getAllNames();
-		
-		final long endTime = System.nanoTime();
-		
-		return endTime - startTime;
-	}
-	
 	@Test
 	public void testIsCodeValid() throws Exception {
 		assertThat(codeIsValid(""), is(false));
@@ -63,4 +48,28 @@ public class AreaTest {
 		assertThat(codeIsValid(new Code("gb")), is(true));
 	}
 	
+	@Test
+	public void testAreaIsOfCorrectType() throws Exception
+	{
+		final Area area0 = getAreaFromCode("DE"), area1 = getAreaFromCode("EU");
+		assertThat(area0 instanceof Country, is(true));
+		assertThat(area1 instanceof Region, is(true));
+	}
+	
+	@Test
+	public void testGetAreaNameFromCode()
+	{
+		assertThat(getAreaCodeFromName("Belgium"), is("BE"));
+		assertThat(getAreaCodeFromName("European Union"), is("EU"));
+	}
+	
+	@Test
+	public void testAreaIndicators() throws Exception
+	{
+		final Area area = getAreaFromCode("FR");
+		final HashMap<Integer, Double> gdp = area.getIndicatorData(Area.GDP, 1999, 2000),
+				inflation = area.getIndicatorData(Area.INFLATION, 1999, 2000);
+		assertThat(gdp.get(1999) > gdp.get(2000), is(true));
+		assertThat(inflation.get(1999) < inflation.get(2000), is(true));
+	}
 }
--- Adam/src/test/java/adam/model/CountryTest.java
+++ Adam/src/test/java/adam/model/CountryTest.java
@@ -9,16 +9,15 @@ import org.junit.Test;
 public class CountryTest {
 	
 	@Test
-	public void testInitialise() {
+	public void testInitialise() throws Exception {
 		final String code = "US";
 		
-		try {
-			final Country country = getCountry(code);
-			
-			assertThat(country.getCode().toUpperCase(), is(code));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		final Country country = getCountry(code);
+		
+		assertThat(country.getCode().toUpperCase(), is(code));
+		assertThat(country.getName(), is("United States"));
+		assertThat(country.getCapitalCity(), is("Washington D.C."));
+		assertThat(country.getRegion(), is("North America"));
 	}
 
 }
