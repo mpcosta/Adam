a9895762f3472192fe64916cb2493a62f580ebd3
Prakash Malam <prakash.malam@kcl.ac.uk>
2016-12-14 14:33:05 +0000

    Add more test cases

--- Adam/src/test/java/adam/model/AreaTest.java
+++ Adam/src/test/java/adam/model/AreaTest.java
@@ -1,18 +1,41 @@
 package adam.model;
 
+import static adam.model.Area.cacheAllNames;
+import static adam.model.Area.codeIsValid;
+import static adam.model.Area.estimateNamesFromFragment;
+import static adam.model.Area.getAllNames;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+import adam.model.Area.Code;
+
 public class AreaTest {
 	
 	@Test
+	public void testEstimateNamesFromFragment() {
+		assertThat(estimateNamesFromFragment("U"), hasItem("United Kingdom"));
+	}
+	
+	@Test
+	public void testGetAllNames() {
+		assertThat(getAllNames().isEmpty(), is(false));
+	}
+	
+	@Test
+	public void testCacheAllNames() {
+		cacheAllNames();
+	}
+	
+	@Test
 	public void testIsCodeValid() throws Exception {
-		assertThat(Area.codeIsValid(""), is(false));
-		assertThat(Area.codeIsValid("g"), is(false));
-		assertThat(Area.codeIsValid("gb"), is(true)); // XXX Fix failing test
-		assertThat(Area.codeIsValid("gbb"), is(false));
+		assertThat(codeIsValid(""), is(false));
+		assertThat(codeIsValid("g"), is(false));
+		assertThat(codeIsValid("gb"), is(true));
+		assertThat(codeIsValid("gbb"), is(false));
+		assertThat(codeIsValid(new Code("gb")), is(true));
 	}
 	
 }
